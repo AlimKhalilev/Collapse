90b7d564a8dda3ded2e18256d9dea96599ce4fd6
Alim <alim-halilko@mail.ru>
2021-11-17 23:08:53 +0300

    доработан основной функционал gui

--- src/js/_gui.js
+++ src/js/_gui.js
@@ -1,69 +1,12 @@
 class GUI {
-    static exceptionList = ["esp", "distance", "distanceToggle"];
+    static espState = false;
+    static distanceState = false;
 
-    static exceptionBranching(handle, handleName, guiContainer) {
-        let playerDistance = +guiContainer.querySelector(".guiPlayer__distance").innerHTML || 231;
-        let playerDistanceToggle = guiContainer.querySelector(".guiColumn__distanceToggle");
-        let playerDistanceOverlay = guiContainer.querySelector(".guiPlayer__distanceOverlay");
-
-        this.changeHandleState(playerDistanceToggle, "distanceOverlay", guiContainer);
-
-        handle.addEventListener("change", () => {
-            if (handleName === "esp") {
-                guiContainer.querySelectorAll(".guiColumn__handle").forEach(handleElem => {
-                    handleElem.checked = handle.checked;
-
-                    let handleSwitchedName = this.getHandleNameByClassList(handleElem.classList);
-                    this.changeHandleState(handleElem, handleSwitchedName, guiContainer);
-                });
-                
-                if (handle.checked) {
-                    guiContainer.querySelector(".guiColumn__progress").classList.remove("disabled");
-
-                    let distance = guiContainer.querySelector(".guiColumn__progress .guiColumn__distance");
-                    distance.value = +distance.attributes.max.value; // при включении присвоить макс. знач. progress
-                }
-                else {
-                    guiContainer.querySelector(".guiColumn__progress").classList.add("disabled");
-                }
-
-                this.changeHandleState(playerDistanceToggle, "distanceOverlay", guiContainer);
-            }
-
-            if (handleName === "distance") {
-
-                guiContainer.querySelectorAll(".guiColumn__handle").forEach(handleElem => {
-                    if (handle.value <= playerDistance) {
-                        if (!handleElem.classList.contains("guiColumn__distanceToggle")) {
-                            handleElem.checked = false;
-
-                            let handleSwitchedName = this.getHandleNameByClassList(handleElem.classList);
-                            this.changeHandleState(handleElem, handleSwitchedName, guiContainer);
-                        }
-
-                    }
-
-                });
-            }
-
-            if (handleName === "distanceToggle") {
-                guiContainer.querySelector(".guiColumn__progress").classList.toggle("disabled");
-                this.changeHandleState(playerDistanceToggle, "distanceOverlay", guiContainer);
-            }
-        });
-    }
-
-    static getHandleNameByClassList(classList) { // убираем лишние классы из списка и превращаем в строку
-        let cleanArr = [...classList].filter(elem => elem.includes("guiColumn__") && !elem.includes("handle") && !elem.includes("progress")).join("");
-        return cleanArr.substr(11, cleanArr.length - 11); // убираем префикс guiColumn__ в строке
-    }
-
-    static changeHandleState(handle, handleName, guiContainer) {
+    static changeHandleState(state, handleName, guiContainer) {
         let handleObject = guiContainer.querySelector(`.guiPlayer__${handleName}`);
-        //console.log("Взаимодействуем с", handleName);
 
         if (handleObject !== null) {
-            if (handle.checked) {
+            if (state) {
                 handleObject.classList.add("active");
             }
             else {
@@ -73,24 +16,46 @@ class GUI {
     }
 
     static initBasicToggler(handle, handleName, guiContainer) {
-        this.changeHandleState(handle, handleName, guiContainer);
+        this.changeHandleState(handle.checked, handleName, guiContainer);
 
         handle.addEventListener("change", () => {
-            guiContainer.querySelector(`.guiPlayer__${handleName}`).classList.toggle("active");
+            if (this.espState && this.distanceState) {
+                this.changeHandleState(handle.checked, handleName, guiContainer);
+            }
+        });
+    }
+
+    static toggleAllPlayerElems(guiContainer, state) {
+        guiContainer.querySelectorAll("[data-handle]").forEach(handle => {
+            let handleState = (state ? handle.checked : false);
+            this.changeHandleState(handleState, handle.dataset.handle, guiContainer)
         });
     }
 
     static initEvents() {
         document.querySelectorAll(".guiContainer").forEach(guiContainer => {
-            guiContainer.querySelectorAll(".guiColumn__handle").forEach(handle => {
-                let handleName = this.getHandleNameByClassList(handle.classList);
 
-                if (this.exceptionList.includes(handleName)) {
-                    this.exceptionBranching(handle, handleName, guiContainer);
-                }
-                else {
-                    this.initBasicToggler(handle, handleName, guiContainer);
-                }
+            let espHandle = guiContainer.querySelector(".guiColumn__toggler.c-checkbox__input");
+            this.espState = espHandle.checked;
+            
+            espHandle.addEventListener("change", () => {
+                this.espState = espHandle.checked;
+                this.toggleAllPlayerElems(guiContainer, this.espState && this.distanceState);
+            });
+
+            let distanceHandle = guiContainer.querySelector(".guiColumn__toggler.guiColumn__progressTrack");
+            let distanceValue = +guiContainer.querySelector(".guiPlayer__distance").innerHTML || 231;
+            this.distanceState = (+distanceHandle.value >= distanceValue);
+            
+            distanceHandle.addEventListener("change", () => {
+                let distanceValue = +guiContainer.querySelector(".guiPlayer__distance").innerHTML || 231;
+                this.distanceState = (+distanceHandle.value >= distanceValue);
+
+                this.toggleAllPlayerElems(guiContainer, this.espState && this.distanceState);
+            });
+
+            guiContainer.querySelectorAll("[data-handle]").forEach(handle => {
+                this.initBasicToggler(handle, handle.dataset.handle, guiContainer);
             });
         });
     }
